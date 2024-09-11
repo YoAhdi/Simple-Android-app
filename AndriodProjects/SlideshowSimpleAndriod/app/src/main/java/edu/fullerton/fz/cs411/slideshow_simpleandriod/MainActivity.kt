e49fb2a73d6bef20bda59d6591f7d14166df8ea8
YoAhdi <ahadahmad.work@gmail.com>
2024-09-11 16:51:19 -0700

    Button added to main 17

--- AndriodProjects/SlideshowSimpleAndriod/app/src/main/java/edu/fullerton/fz/cs411/slideshow_simpleandriod/MainActivity.kt
+++ AndriodProjects/SlideshowSimpleAndriod/app/src/main/java/edu/fullerton/fz/cs411/slideshow_simpleandriod/MainActivity.kt
@@ -1,12 +1,17 @@
 package edu.fullerton.fz.cs411.slideshow_simpleandriod
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var previousButton: Button
+    private lateinit var nextButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
