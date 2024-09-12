cb3552dec8ee6d26ef1727b153427e6c5deedf92
YoAhdi <ahadahmad.work@gmail.com>
2024-09-11 17:32:54 -0700

    Done

--- AndriodProjects/SlideshowSimpleAndriod/app/src/main/java/edu/fullerton/fz/cs411/slideshow_simpleandriod/MainActivity.kt
+++ AndriodProjects/SlideshowSimpleAndriod/app/src/main/java/edu/fullerton/fz/cs411/slideshow_simpleandriod/MainActivity.kt
@@ -1,6 +1,7 @@
 package edu.fullerton.fz.cs411.slideshow_simpleandriod
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -16,10 +17,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        this.previousButton = this.findViewById(R.id.previousButton)
+        this.nextButton = this.findViewById(R.id.nextButton)
+
+        this.previousButton.setOnClickListener{ view: View ->
+            println("Previous button clicked!")
+        }
+        this.nextButton.setOnClickListener { view:View ->
+            println("Next Button clicked!")
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
     }
 }
